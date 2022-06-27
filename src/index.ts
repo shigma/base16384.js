817d5c3ac6873ecfe2db52c09d328fc2c275b295
Shigma <shigma10826@gmail.com>
2022-06-27 19:28:10 +0800

    feat: support decode

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,58 @@
+type TypedArray = Uint8Array | Uint16Array
+
+function align(input: TypedArray, output: TypedArray, sWidth: number, tWidth: number, sOffset: number, tOffset: number) {
+  let offset = 0
+  let rest = 0
+  let i = 0, j = 0
+  const mask = (1 << tWidth) - 1
+  while (i < input.length) {
+    const char = input[i] - sOffset
+    offset += sWidth
+    while (offset >= tWidth) {
+      offset -= tWidth
+      output[j++] = rest + (char >> offset) + tOffset
+      if (j === output.length) return
+      rest = 0
+    }
+    rest += (char << (tWidth - offset)) & mask
+    i++
+  }
+  if (offset) {
+    output[j++] = rest + tOffset
+  }
+}
+
+export function toUint8Array(source: string) {
+  return new TextEncoder().encode(source)
+}
+
 export function encode(input: string | Uint8Array) {
   if (typeof input === 'string') {
-    input = new TextEncoder().encode(input)
+    input = toUint8Array(input)
   }
 
-  let offset = 0, rest = 0, output = ''
-  for (const byte of input) {
-    offset += 8
-    if (offset < 14) {
-      rest += (byte << (14 - offset)) & 0x3fff
-      continue
-    }
-    offset -= 14
-    output += String.fromCharCode(rest + (byte >> offset) + 0x4e00)
-    rest = (byte << (14 - offset)) & 0x3fff
+  const output = new Uint16Array(Math.ceil(input.length * 4 / 7) + 1)
+  align(input, output, 8, 14, 0, 0x4e00)
+  output[output.length - 1] = input.length % 7 + 0x3d00
+  return output
+}
+
+export function toUint16Array(source: string) {
+  const input = new Uint16Array(source.length)
+  for (let i = 0; i < source.length; i++) {
+    input[i] = source.charCodeAt(i)
+  }
+  return input
+}
+
+export function decode(input: string | Uint16Array) {
+  if (typeof input === 'string') {
+    input = toUint16Array(input)
   }
 
-  if (offset) {
-    output += String.fromCharCode(rest + 0x4e00)
-    output += String.fromCharCode(input.length % 7 + 0x3d00)
-  }
-
+  const length = input.length - 1
+  const residue = (input[length] - 0x3d00) || 7
+  const output = new Uint8Array(Math.floor((length - 1) / 4) * 7 + residue)
+  align(input, output, 14, 8, 0x4e00, 0)
   return output
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,15 +1,28 @@
 import assert from 'assert'
-import { encode } from '../src'
+import { decode, encode, toUint16Array, toUint8Array } from '../src'
 
 it('encode', () => {
-  assert.equal(encode(''), '')
-  assert.equal(encode('1'), '婀㴁')
-  assert.equal(encode('12'), '婌渀㴂')
-  assert.equal(encode('123'), '婌焰㴃')
-  assert.equal(encode('1234'), '婌焳帀㴄')
-  assert.equal(encode('12345'), '婌焳廔㴅')
-  assert.equal(encode('123456'), '婌焳廔萀㴆')
-  assert.equal(encode('1234567'), '婌焳廔萷')
-  assert.equal(encode('12345678'), '婌焳廔萷尀㴁')
-  assert.equal(encode('123456789'), '婌焳廔萷導帀㴂')
+  assert.deepEqual(encode(''), toUint16Array('㴀'))
+  assert.deepEqual(encode('1'), toUint16Array('婀㴁'))
+  assert.deepEqual(encode('12'), toUint16Array('婌渀㴂'))
+  assert.deepEqual(encode('123'), toUint16Array('婌焰㴃'))
+  assert.deepEqual(encode('1234'), toUint16Array('婌焳帀㴄'))
+  assert.deepEqual(encode('12345'), toUint16Array('婌焳廔㴅'))
+  assert.deepEqual(encode('123456'), toUint16Array('婌焳廔萀㴆'))
+  assert.deepEqual(encode('1234567'), toUint16Array('婌焳廔萷㴀'))
+  assert.deepEqual(encode('12345678'), toUint16Array('婌焳廔萷尀㴁'))
+  assert.deepEqual(encode('123456789'), toUint16Array('婌焳廔萷導帀㴂'))
+})
+
+it('decode', () => {
+  assert.deepEqual(decode('㴀'), toUint8Array(''))
+  assert.deepEqual(decode('婀㴁'), toUint8Array('1'))
+  assert.deepEqual(decode('婌渀㴂'), toUint8Array('12'))
+  assert.deepEqual(decode('婌焰㴃'), toUint8Array('123'))
+  assert.deepEqual(decode('婌焳帀㴄'), toUint8Array('1234'))
+  assert.deepEqual(decode('婌焳廔㴅'), toUint8Array('12345'))
+  assert.deepEqual(decode('婌焳廔萀㴆'), toUint8Array('123456'))
+  assert.deepEqual(decode('婌焳廔萷㴀'), toUint8Array('1234567'))
+  assert.deepEqual(decode('婌焳廔萷尀㴁'), toUint8Array('12345678'))
+  assert.deepEqual(decode('婌焳廔萷導帀㴂'), toUint8Array('123456789'))
 })
